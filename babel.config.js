cf3069945abb2c1bd700bd08ecbca382193c197e
Jason Blanchard <jas.blanchard@gmail.com>
2020-01-02 13:55:25 -0500

    First commit

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [
+    "@babel/plugin-proposal-optional-chaining"
+  ],
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-typescript',
+    '@babel/preset-react'
+  ],
+};
