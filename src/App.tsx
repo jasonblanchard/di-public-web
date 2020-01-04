cc3fe52124df72cd5b1ddddf73b509bcbc082b0a
Jason Blanchard <jas.blanchard@gmail.com>
2020-01-04 00:53:29 -0500

    fix route

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/login" exact>
+        <Route path="/anon/login" exact>
           <LoginPage />
         </Route>
       </Switch>
